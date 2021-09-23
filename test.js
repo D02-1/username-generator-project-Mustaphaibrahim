83095791b1ee0a3f85f78b8f5e32618306e07a53
Mustapha <stexe@msn.com>
2021-09-23 14:01:28 +0200

    final commit

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+//Wir importieren unsere App
 const usernameGenerator = require('./src/index.js');
 
+//Wir führen die funktion der app aus, mit dem alias das wir vergeben haben
 const username = usernameGenerator.createUserName(50000);
 
 console.log(username);
