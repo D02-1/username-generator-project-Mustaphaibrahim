824722461d602f8b5900df187625f65b6edca29a
Mustapha <stexe@msn.com>
2021-09-23 14:02:05 +0200

    final commit

--- test.js
+++ test.js
@@ -4,4 +4,5 @@ const usernameGenerator = require('./src/index.js');
 //Wir führen die funktion der app aus, mit dem alias das wir vergeben haben
 const username = usernameGenerator.createUserName(50000);
 
+//wir geben das ergebnis aus
 console.log(username);
